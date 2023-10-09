68fe8d2b62e89c7eb1f5390a2904d085113fc43a
ksulaoja <kristjanmarcus@gmail.com>
2023-10-09 15:19:20 +0300

    frontend post zoom bot data to back.

--- project/front/src/components/ZoomForm.tsx
+++ project/front/src/components/ZoomForm.tsx
@@ -3,12 +3,13 @@ import './styles.css';
 
 export const ZoomForm = () => {
   const [formData, setFormData] = useState({
-    name: '',
     meetingId: '',
-    meetingPassword: '',
+    meetingPw: '',
     startTime: '',
-    meetingLength: ''
+    recordingLength: 0,
+    userEmail: ''
   });
+  const [showPw, setShowPw] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -18,9 +19,29 @@ export const ZoomForm = () => {
     });
   };
 
+  const handleMeetingIdChange = (e) => {
+    handleChange(e);
+    let isnum = /^\d+$/.test(e.target.value);
+    setShowPw(isnum);
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    let date = new Date(formData.startTime);
+    let isoDate = date.toISOString();
+    formData.startTime = isoDate;
     console.log('Form Data:', formData);
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    };
+
+    fetch('http://localhost:8080/recordings', requestOptions)
+    .then(response => response.json())
+    .then(data => console.log(data));
   };
 
   return (
@@ -28,35 +49,25 @@ export const ZoomForm = () => {
       <h1>Zoom Analyzer</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
           <label>Meeting ID:</label>
           <input
             type="text"
             name="meetingId"
             value={formData.meetingId}
-            onChange={handleChange}
+            onChange={handleMeetingIdChange}
             required
           />
         </div>
-        <div>
+        {showPw && <div>
           <label>Meeting Password:</label>
           <input
             type="text"
-            name="meetingPassword"
-            value={formData.meetingPassword}
+            name="meetingPw"
+            value={formData.meetingPw}
             onChange={handleChange}
             required
           />
-        </div>
+        </div>}
         <div>
           <label>Starting Time:</label>
           <input
@@ -71,8 +82,18 @@ export const ZoomForm = () => {
           <label>Meeting Length (in minutes):</label>
           <input
             type="number"
-            name="meetingLength"
-            value={formData.meetingLength}
+            name="recordingLength"
+            value={formData.recordingLength}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            name="userEmail"
+            value={formData.userEmail}
             onChange={handleChange}
             required
           />
--- scheduler/src/main/java/ee/taltech/zoomalyzer/controllers/RecordingController.java
+++ scheduler/src/main/java/ee/taltech/zoomalyzer/controllers/RecordingController.java
@@ -11,12 +11,7 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.MalformedURLException;
 import java.nio.file.Path;
@@ -41,6 +36,7 @@ public class RecordingController {
     }
 
     @PostMapping
+    @CrossOrigin(origins = "*")
     public RecordingDto scheduleRecording(@RequestBody RecordingDto dto) {
         Recording recording = toRecording(dto);
         Recording savedRec = recordingService.save(recording);
